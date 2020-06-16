bcd8ffbc2a618d1cb752cefe67b8fcb873fe307c
Irvin Reyes <reyes.yerp@gmail.com>
2020-06-16 11:23:34 -0500

    Cleanup

--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,14 @@ import axios from "axios";
 
 function Home({ products }) {
   console.log(products);
-  // useEffect(() => {
-  //   getProducts();
-  // }, []);
-
-  // async function getProducts() {}
 
   return <>home</>;
 }
 
 Home.getInitialProps = async (ctx) => {
-  // fetch data on the server
   const url = "http://localhost:3000/api/products";
   const response = await axios.get(url);
   return { products: response.data };
-
-  // return response data as an object
-
-  // note: this object will be merged with existing props
 };
 
 export default Home;
