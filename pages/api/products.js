940d2c125fbb8a9b0fbd147c1fe94535b6d0a583
Irvin Reyes <reyes.yerp@gmail.com>
2020-06-16 11:23:20 -0500

    Connect database

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,4 +1,7 @@
 import products from "../../public/static/products.json";
+import connectDb from "../../utils/connectDb";
+
+connectDb();
 
 export default (req, res) => {
   res.status(200).json(products);
