be3b7d39ed3a5415fe54b31a4856093e310dc34f
Irvin Reyes <reyes.yerp@gmail.com>
2020-06-17 10:14:09 -0500

    Build Product cards

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import { useEffect } from "react";
 import axios from "axios";
+import ProductList from "../components/Index/ProductList";
 
 function Home({ products }) {
-  console.log(products);
-
-  return <>home</>;
+  return <ProductList products={products} />;
 }
 
 Home.getInitialProps = async (ctx) => {
