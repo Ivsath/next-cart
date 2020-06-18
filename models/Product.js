37142bfa8588edee7b729d6526f24ead02864258
Irvin Reyes <reyes.yerp@gmail.com>
2020-06-18 16:44:31 -0500

    Fix sku generation

--- models/Product.js
+++ models/Product.js
@@ -15,7 +15,7 @@ const ProductSchema = new mongoose.Schema({
   sku: {
     type: String,
     unique: true,
-    default: shortid.generate(),
+    default: shortid.generate,
   },
   description: {
     type: String,
