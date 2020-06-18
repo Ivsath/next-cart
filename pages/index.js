2d7557856e894c756310e2578edf7daa5a310203
Irvin Reyes <reyes.yerp@gmail.com>
2020-06-17 21:59:38 -0500

    Use a base url helper

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import { useEffect } from "react";
 import axios from "axios";
 import ProductList from "../components/Index/ProductList";
+import baseUrl from "../utils/baseUrl";
 
 function Home({ products }) {
   return <ProductList products={products} />;
 }
 
 Home.getInitialProps = async (ctx) => {
-  const url = "http://localhost:3000/api/products";
+  const url = `${baseUrl}/api/products`;
   const response = await axios.get(url);
   return { products: response.data };
 };
