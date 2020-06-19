b9a63e85340e6beb6f43d132c5a0e78d7cb3dd15
Irvin Reyes <reyes.yerp@gmail.com>
2020-06-18 23:58:39 -0500

    Build login and signup forms

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,107 @@
+import { useState, useEffect } from "react";
+import {
+  Button,
+  Form,
+  Icon,
+  Message,
+  Segment,
+  Divider,
+} from "semantic-ui-react";
+import Link from "next/link";
+import catchErrors from "../utils/catchErrors";
+
+const INITIAL_USER = {
+  email: "",
+  password: "",
+};
+
 function Login() {
-  return <>login</>;
+  const [user, setUser] = useState(INITIAL_USER);
+  const [disabled, setDisabled] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const isUser = Object.values(user).every((el) => Boolean(el));
+    isUser ? setDisabled(false) : setDisabled(true);
+  }, [user]);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUser((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      setLoading(true);
+      setError("");
+      console.log(user);
+      // TODO: Make request to sign up user
+    } catch (error) {
+      catchErrors(error, setError);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <Message
+        attached
+        icon="privacy"
+        header="Welcome Back!"
+        content="Log in with email and password"
+        color="blue"
+      />
+      <Divider hidden />
+      <Form error={Boolean(error)} loading={loading} onSubmit={handleSubmit}>
+        <Message error header="Oops!" content={error} />
+        <Segment>
+          <Form.Input
+            fluid
+            icon="envelope"
+            iconPosition="left"
+            label="Email"
+            placeholder="Email"
+            required
+            name="email"
+            type="email"
+            value={user.email}
+            onChange={handleChange}
+          />
+          <Form.Input
+            fluid
+            icon="lock"
+            iconPosition="left"
+            label="Password"
+            placeholder="Password"
+            required
+            name="password"
+            type="password"
+            value={user.password}
+            onChange={handleChange}
+          />
+          <Button
+            disabled={disabled || loading}
+            icon="sign in"
+            type="submit"
+            color="orange"
+            content="Login"
+          />
+        </Segment>
+      </Form>
+      <Divider hidden />
+      <Message attached="bottom" warning>
+        <Icon name="help" />
+        New user?{" "}
+        <Link href="/signup">
+          <a>Sign up here</a>
+        </Link>{" "}
+        instead.
+      </Message>
+    </>
+  );
 }
 
 export default Login;
--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,119 @@
+import { useState, useEffect } from "react";
+import {
+  Button,
+  Form,
+  Icon,
+  Message,
+  Segment,
+  Divider,
+} from "semantic-ui-react";
+import Link from "next/link";
+import catchErrors from "../utils/catchErrors";
+
+const INITIAL_USER = {
+  name: "",
+  email: "",
+  password: "",
+};
+
 function Signup() {
-  return <>signup</>;
+  const [user, setUser] = useState(INITIAL_USER);
+  const [disabled, setDisabled] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const isUser = Object.values(user).every((el) => Boolean(el));
+    isUser ? setDisabled(false) : setDisabled(true);
+  }, [user]);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUser((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      setLoading(true);
+      setError("");
+      console.log(user);
+      // TODO: Make request to sign up user
+    } catch (error) {
+      catchErrors(error, setError);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <Message
+        attached
+        icon="settings"
+        header="Get Started!"
+        content="Create a new account"
+        color="teal"
+      />
+      <Divider hidden />
+      <Form error={Boolean(error)} loading={loading} onSubmit={handleSubmit}>
+        <Message error header="Oops!" content={error} />
+        <Segment>
+          <Form.Input
+            fluid
+            icon="user"
+            iconPosition="left"
+            label="Name"
+            placeholder="Name"
+            required
+            name="name"
+            value={user.name}
+            onChange={handleChange}
+          />
+          <Form.Input
+            fluid
+            icon="envelope"
+            iconPosition="left"
+            label="Email"
+            placeholder="Email"
+            required
+            name="email"
+            type="email"
+            value={user.email}
+            onChange={handleChange}
+          />
+          <Form.Input
+            fluid
+            icon="lock"
+            iconPosition="left"
+            label="Password"
+            placeholder="Password"
+            required
+            name="password"
+            type="password"
+            value={user.password}
+            onChange={handleChange}
+          />
+          <Button
+            disabled={disabled || loading}
+            icon="signup"
+            type="submit"
+            color="orange"
+            content="Signup"
+          />
+        </Segment>
+      </Form>
+      <Divider hidden />
+      <Message attached="bottom" warning>
+        <Icon name="help" />
+        Existing user?{" "}
+        <Link href="login">
+          <a>Log in here</a>
+        </Link>{" "}
+        instead.
+      </Message>
+    </>
+  );
 }
 
 export default Signup;
