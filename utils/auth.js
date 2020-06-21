fb17543bb40422ba6bea0395430df04501e687ab
Irvin Reyes <reyes.yerp@gmail.com>
2020-06-21 16:38:22 -0500

    Get current user from token

--- utils/auth.js
+++ utils/auth.js
@@ -5,3 +5,14 @@ export const handleLogin = (token) => {
   cookie.set("token", token);
   Router.push("/account");
 };
+
+export const redirectUser = (ctx, location) => {
+  if (ctx.req) {
+    // We are on the server
+    ctx.res.writeHead(302, { Location: location });
+    ctx.res.end();
+  } else {
+    // We are on the client
+    Router.push(location);
+  }
+};
