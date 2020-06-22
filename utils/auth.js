c365b47be6d51df0c0b80dfe82327a3d65bed171
Irvin Reyes <reyes.yerp@gmail.com>
2020-06-22 12:38:42 -0500

    Universal logout using localstorage

--- utils/auth.js
+++ utils/auth.js
@@ -19,5 +19,6 @@ export const redirectUser = (ctx, location) => {
 
 export const handleLogout = () => {
   cookie.remove("token");
+  window.localStorage.setItem("logout", Date.now());
   Router.push("/login");
 };
