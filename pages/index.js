29e7aa530981f0fdc8d1a294a31bc4891b514026
Irvin Reyes <reyes.yerp@gmail.com>
2020-07-20 17:56:03 -0500

    Add pagination to the products list

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,27 @@
 import { useEffect } from "react";
 import axios from "axios";
+
 import ProductList from "../components/Index/ProductList";
+import ProductPagination from "../components/Index/ProductPagination";
 import baseUrl from "../utils/baseUrl";
 
-function Home({ products }) {
-  return <ProductList products={products} />;
+function Home({ products, totalPages }) {
+  return (
+    <>
+      <ProductList products={products} />
+      <ProductPagination totalPages={totalPages} />
+    </>
+  );
 }
 
 Home.getInitialProps = async (ctx) => {
+  const page = ctx.query.page ? ctx.query.page : "1";
+  const size = 9;
   const url = `${baseUrl}/api/products`;
-  const response = await axios.get(url);
-  return { products: response.data };
+  const payload = { params: { page, size } };
+  const response = await axios.get(url, payload);
+
+  return response.data;
 };
 
 export default Home;
