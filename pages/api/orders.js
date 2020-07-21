843e644a8180964013a85dbdf348d6821ce8f65c
Irvin Reyes <reyes.yerp@gmail.com>
2020-07-21 18:03:20 -0500

    Create orders endpoint

---
+++ pages/api/orders.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+
+import Order from "../../models/Order";
+import Product from "../../models/Product";
+import connectDb from "../../utils/connectDb";
+
+connectDb();
+
+export default async (req, res) => {
+  try {
+    const { userId } = jwt.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
+    const orders = await Order.find({ user: userId }).populate({
+      path: "products.product",
+      model: Product,
+    });
+
+    res.status(200).json({ orders });
+  } catch (error) {
+    console.error(error);
+    res.status(403).send("Please login again");
+  }
+};
