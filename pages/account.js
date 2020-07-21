8b38860d2076d14fd0564239f121f445e0e0fdf9
Irvin Reyes <reyes.yerp@gmail.com>
2020-07-21 18:02:58 -0500

    Create account page

--- pages/account.js
+++ pages/account.js
@@ -1,5 +1,30 @@
-function Account() {
-  return <>account</>;
+import axios from "axios";
+import { parseCookies } from "nookies";
+
+import AccountHeader from "../components/Account/AccountHeader";
+import AccountOrders from "../components/Account/AccountOrders";
+import baseUrl from "../utils/baseUrl";
+
+function Account({ user, orders }) {
+  return (
+    <>
+      <AccountHeader {...user} />
+      <AccountOrders orders={orders} />
+    </>
+  );
 }
+
+Account.getInitialProps = async (ctx) => {
+  const { token } = parseCookies(ctx);
+  if (!token) {
+    return { orders: [] };
+  }
+
+  const payload = { headers: { Authorization: token } };
+  const url = `${baseUrl}/api/orders`;
+  const response = await axios.get(url, payload);
+
+  return response.data;
+};
 
 export default Account;
