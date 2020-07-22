f96608decfc2d1b7b70b750e0945ae602f329595
Irvin Reyes <reyes.yerp@gmail.com>
2020-07-21 19:18:15 -0500

    Create user permissions component

--- pages/account.js
+++ pages/account.js
@@ -3,6 +3,7 @@ import { parseCookies } from "nookies";
 
 import AccountHeader from "../components/Account/AccountHeader";
 import AccountOrders from "../components/Account/AccountOrders";
+import AccountPermissions from "../components/Account/AccountPermissions";
 import baseUrl from "../utils/baseUrl";
 
 function Account({ user, orders }) {
@@ -10,6 +11,7 @@ function Account({ user, orders }) {
     <>
       <AccountHeader {...user} />
       <AccountOrders orders={orders} />
+      {user.role === "root" && <AccountPermissions />}
     </>
   );
 }
